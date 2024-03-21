a3cbcddb4f5779280014224c20051099fe0ab978
Naman Sharma <namansharma3007c@gmail.com>
2024-03-21 13:11:32 +0530

    api.js and redis.js changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-// Import the process polyfill
-// import 'react-refresh/runtime';
-// import process from '@pmmmwh/react-refresh-webpack-plugin/cjs/react-refresh-process.js';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
