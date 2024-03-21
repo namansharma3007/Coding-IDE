918b5fd199f1b7f3531c97654e73cf54df46d205
Naman Sharma <namansharma3007c@gmail.com>
2024-03-21 11:24:55 +0530

    added _redirects

--- src/redis/redis.js
+++ src/redis/redis.js
@@ -1,8 +1,8 @@
 import { Redis } from '@upstash/redis'
   
 const redis = new Redis({
-  url: process.env.VITE_UPSTASH_REDIS_REST_URL,
-  token: process.env.VITE_UPSTASH_REDIS_REST_TOKEN,
+  url: import.meta.env.VITE_UPSTASH_REDIS_REST_URL,
+  token: import.meta.env.VITE_UPSTASH_REDIS_REST_TOKEN,
 })
 
 export default redis;
