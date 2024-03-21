bb02bfc3c40962a67c00928f324651cf0f357536
Naman Sharma <namansharma3007c@gmail.com>
2024-03-21 15:16:17 +0530

    removed necessary packages

--- src/redis/redis.js
+++ src/redis/redis.js
@@ -4,9 +4,4 @@ const redis = new Redis({
   url: import.meta.env.VITE_UPSTASH_REDIS_REST_URL,
   token: import.meta.env.VITE_UPSTASH_REDIS_REST_TOKEN,
 })
-// const redis = new Redis({
-//   url: "https://able-ape-31684.upstash.io",
-//   token: "AXvEACQgMzEwN2ZjNzUtYTdlYS00ZDc4LWE5NjEtOTk2ZDIwODdmZjlmMDUwNTM3ZGU5NDUyNDg5NTgwNjMxN2MwMDRmOTE2MWU=",
-// })
-
 export default redis;
--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,3 @@
-// import { defineConfig, loadEnv } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd(), '');
-
-//   return {
-//     plugins: [react()],
-
-//     define: {
-//       'import.meta.env': env
-//     },
-//   };
-// })
-
-// import { defineConfig } from 'vite'
-// import envCompatible from 'vite-plugin-env-compatible'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react(),
-//   envCompatible(),
-//   ],
-// })
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
