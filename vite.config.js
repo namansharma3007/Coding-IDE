d08a612ffd0bdeb310f011c881b1ba7ea0c3141a
Naman Sharma <namansharma3007c@gmail.com>
2024-03-21 14:54:05 +0530

    added vite-plugin-env-compatible

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,25 @@
-// import { defineConfig, loadEnv } from "vite";
+// import { defineConfig, loadEnv } from 'vite';
 // import react from '@vitejs/plugin-react';
 
-
-// // https://vitejs.dev/config/
 // export default defineConfig(({ mode }) => {
 //   const env = loadEnv(mode, process.cwd(), '');
-  
+
 //   return {
 //     plugins: [react()],
+
 //     define: {
-//       'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
-//       'process.env.VITE_X_RAPID_API_KEY': JSON.stringify(env.VITE_X_RAPID_API_KEY),
-//       'process.env.VITE_X_RAPID_API_HOST': JSON.stringify(env.VITE_X_RAPID_API_HOST),
-//       'process.env.VITE_UPSTASH_REDIS_REST_URL': JSON.stringify(env.VITE_UPSTASH_REDIS_REST_URL),
-//       'process.env.VITE_UPSTASH_REDIS_REST_TOKEN': JSON.stringify(env.VITE_UPSTASH_REDIS_REST_TOKEN),
+//       'import.meta.env': env
 //     },
 //   };
-// });
+// })
 
-import { defineConfig , loadEnv} from 'vite'
+import { defineConfig } from 'vite'
+import envCompatible from 'vite-plugin-env-compatible'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    
-    return {
-      plugins: [react()],
-      define: {
-        'process.env':env
-      },
-    };
-  });
-  
+export default defineConfig({
+  plugins: [react(),
+  envCompatible(),
+  ],
+})
