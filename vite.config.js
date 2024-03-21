d7331fc6b0808db2f75c4d3bb0a966a7d2226774
Naman Sharma <namansharma3007c@gmail.com>
2024-03-21 12:19:36 +0530

    changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
+import { defineConfig, loadEnv } from "vite";
+// import dotenv from 'dotenv';
+// dotenv.config();
 import react from '@vitejs/plugin-react'
-dotenv.config();
 
+const env = loadEnv(mode, process.cwd(), "")
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
